01165dd5d2c01199e1f32299363bd33d470a1092
Andrey Arzamaskov <arzamaskov@gmail.com>
2026-01-10 17:54:19 +0400

    feat(ui): add swipe gesture to open/close mobile sidebar

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -19,3 +19,35 @@ function closeSidebar() {
 toggle.addEventListener('click', openSidebar);
 closeBtn.addEventListener('click', closeSidebar);
 overlay.addEventListener('click', closeSidebar);
+
+// Swipe to open/close sidebar
+let touchStartX = 0;
+let touchStartY = 0;
+let touchEndX = 0;
+
+document.addEventListener('touchstart', (e) => {
+    touchStartX = e.changedTouches[0].screenX;
+    touchStartY = e.changedTouches[0].screenY;
+}, { passive: true });
+
+document.addEventListener('touchend', (e) => {
+    touchEndX = e.changedTouches[0].screenX;
+    const touchEndY = e.changedTouches[0].screenY;
+
+    const diffX = touchEndX - touchStartX;
+    const diffY = Math.abs(touchEndY - touchStartY);
+
+    // Swipe must be horizontal (diffX > diffY) and at least 80px
+    if (Math.abs(diffX) > 80 && diffX > diffY) {
+        const isSidebarOpen = !sidebar.classList.contains('-translate-x-full');
+
+        // Swipe right from left edge (< 50px) to open
+        if (diffX > 0 && touchStartX < 50 && !isSidebarOpen) {
+            openSidebar();
+        }
+        // Swipe left to close
+        else if (diffX < 0 && isSidebarOpen) {
+            closeSidebar();
+        }
+    }
+}, { passive: true });
